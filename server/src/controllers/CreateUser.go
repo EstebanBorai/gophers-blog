a244dab9708f4562425b82fcbce01d7c018275cd
Esteban Borai <estebanborai@outlook.com>
2019-05-30 23:39:13 -0300

    Refactor CreateUser controller

--- server/src/controllers/CreateUser.go
+++ server/src/controllers/CreateUser.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
   "time"
-  uuid "github.com/google/uuid"
-  models "models"
   "encoding/json"
+  models "models"
+  helpers "helpers"
+  eh "lib/error_handlers"
   "github.com/jinzhu/gorm"
+  "github.com/gin-gonic/gin"
+  uuid "github.com/google/uuid"
   _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
@@ -14,21 +17,15 @@ type Payload struct {
   Password string
 }
 
-func CreateUser(encodedUser string) (response string, err error) {
-  db, err := gorm.Open("mysql", "root:root@tcp(songs-share-db)/songs-share?charset=utf8mb4&parseTime=True&loc=Local")
-
-  if err != nil {
-    return "", err
-  } else {
-    db.AutoMigrate(&models.User{})
-  }
-
-  defer db.Close()
-
+func CreateUser(c *gin.Context) {
   var id string = uuid.New().String()
   var decodedPayload Payload
+  var encodedUser string = helpers.ContextRequestBody(c)
 
-  json.Unmarshal([]byte(encodedUser), &decodedPayload)
+  if err := json.Unmarshal([]byte(encodedUser), &decodedPayload); err != nil {
+    var msg string = "Invalid JSON " + err.Error()
+    eh.ResponseWithError(c, 400, msg)
+  }
 
   user := models.User {
     Id: id,
@@ -39,20 +36,25 @@ func CreateUser(encodedUser string) (response string, err error) {
     DateJoined: time.Now(),
   }
 
+  db, err := gorm.Open("mysql", "root:root@tcp(songs-share-db)/songs-share?charset=utf8mb4&parseTime=True&loc=Local")
+
+  if err != nil {
+    eh.ResponseWithError(c, 500, "Unable to connect to the database")
+  } else {
+    // TODO: Automigrate on Init
+    db.AutoMigrate(&models.User{})
+  }
+
+  defer db.Close()
+  
   db.NewRecord(user)
   db.Create(&user)
-
+  
   _, passwordError := CreatePassword(decodedPayload.Password, user.Id)
 
   if passwordError != nil {
-    return "", passwordError
+    eh.ResponseWithError(c, 400, "Invalid Password")
   }
 
-  jsonEncoded, err := json.Marshal(user)
-
-  if err != nil {
-    return "", err
-  }
-
-  return string(jsonEncoded), nil
+  c.JSON(200, user)
 }
--- server/src/routes/routes.go
+++ server/src/routes/routes.go
@@ -12,36 +12,10 @@ func StartServer() {
   router.Use(cors.Default())
 
   router.POST("/api/v1/users", func (c *gin.Context) {
-    buf := make([]byte, 1024)
-    num, _ := c.Request.Body.Read(buf)
-    reqBody := string(buf[0:num])
-
-    response, err := controllers.CreateUser(reqBody)
-
-    if err != nil {
-      c.JSON(500, err)
-    } else {
-      c.JSON(200, response)
-    }
+    controllers.CreateUser(c)
   })
 
   router.POST("/login", func (c *gin.Context) {
-    // buf := make([]byte, 1024)
-    // num, _ := c.Request.Body.Read(buf)
-    // reqBody := string(buf[0:num])
-
-    // cookie := controllers.LogIn(reqBody)
-
-    // c.SetCookie(
-    //   cookie.Name,
-    //   cookie.Value,
-    //   3600,
-    //   "/",
-    //   "localhost",
-    //   false,
-    //   true,
-    // )
-    // controllers.LogIn(c)
     controllers.LogIn(c)
   })
 
