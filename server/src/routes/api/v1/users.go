3ac08a386c296c14b88ce4e620476841a738c8d0
Esteban Borai <estebanborai@outlook.com>
2019-06-20 22:37:55 -0300

    Update user fields

--- server/src/routes/api/v1/users.go
+++ server/src/routes/api/v1/users.go
@@ -1,35 +1,35 @@
 package routes
 
 import (
-  "github.com/gin-gonic/gin"
-  "github.com/appleboy/gin-jwt"
-  controllers "github.com/estebanborai/songs-share-server/controllers/users"
-  
+	"github.com/gin-gonic/gin"
+	"github.com/appleboy/gin-jwt"
+	controllers "github.com/estebanborai/songs-share-server/controllers/users"
+	
 )
 
 func UsersRoute(r *gin.Engine, authMiddleware *jwt.GinJWTMiddleware) {
-  users := r.Group("api/v1/users")
+	users := r.Group("api/v1/users")
 
-  users.Use(authMiddleware.MiddlewareFunc())
-  {
-    users.GET("/", func (c *gin.Context) {
-      controllers.ReadUsers(c)
-    })
+	users.Use(authMiddleware.MiddlewareFunc())
+	{
+		users.GET("/", controllers.ReadUsers)
 
-    users.GET("/id/:id", func (c *gin.Context) {
-      id := c.Param("id")
+		users.GET("/id/:id", func (c *gin.Context) {
+			// TODO: Refactor use single parameter function
+			id := c.Param("id")
 
-      controllers.FindUserById(c, id)
-    })
+			controllers.FindUserById(c, id)
+		})
 
-    users.GET("/username/:username", func (c *gin.Context) {
-      userName := c.Param("username")
+		users.GET("/username/:username", func (c *gin.Context) {
+			// TODO: Refactor use single parameter function
+			userName := c.Param("username")
 
-      controllers.FindUserByUserName(c, userName)
-    })
+			controllers.FindUserByUserName(c, userName)
+		})
 
-    users.PATCH("/update/password", func (c *gin.Context) {
-      controllers.UpdatePassword(c)
-    })
-  }
+		users.PATCH("/id/:id", controllers.UpdateUser)
+
+		users.PATCH("/update/password", controllers.UpdatePassword)
+	}
 }
